2c8244b620e2d9b31ac7824714d515972ffde30d
Desmond <desmond@acsurf.net>
2013-12-09 15:51:22 -0500

    removing tests

--- app/scripts/style.js
+++ app/scripts/style.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	var clockWidth = parseInt($(".clock").css('width'));
 	var clockHeight = parseInt($(".clock").css('height'));
-	$(".clock").css('left', "calc(50%-" + clockWidth/2 + "px)");
-	$(".clock").css('top', "calc(50%-" + clockHeight/2 + "px)");
+	$(".clock").css('margin-left', -clockWidth/2 + "px");
+	$(".clock").css('margin-top', -clockHeight/2 + "px");
+	// $(".clock").css('left', "calc(50%-" + clockWidth/2 + "px)");
+	// $(".clock").css('top', "calc(50%-" + clockHeight/2 + "px)");
 
 
 	//fade in clock
